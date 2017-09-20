5235db515d43b03a487c1b0681b7ed8ee60e5aaa
liudonghai <liudonghai@inspur.com>
2017-09-20 13:52:35 +0800

    合并公司和在家写的脚本

--- file_search.sh
+++ file_search.sh
@@ -14,6 +14,18 @@ function file_search()
 		fi
 	done 
 }
+function getdir(){
+    for element in `ls $1`
+    do  
+        dir_or_file=$1"/"$element
+        if [ -d $dir_or_file ]
+        then 
+            getdir $dir_or_file
+        else
+            echo $dir_or_file
+        fi  
+    done
+}
 
 echo "This is the begin of the srcript"
 echo "the paramter is $1"
